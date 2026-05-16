526292b5223406018a99fb02022a3adaed3e6207
Roni Egbu <ronniiii@yahoo.com>
2026-05-16 22:07:03 +0100

    feat: update ALLOWED_ORIGINS to include production URL for CORS configuration

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     DB_URL: str
     APP_ENV: str = "development"
     SECRET_KEY: str = "change-me"
-    ALLOWED_ORIGINS: str = "http://localhost:3000, http://localhost:5173"
+    ALLOWED_ORIGINS: str = "http://localhost:3000, http://localhost:5173,https://requirements-system.vercel.app"
     HF_SPACE_URL: str = "https://roniegbu-reqgen-nlp.hf.space"
     HF_MODEL_NAME: str = "deepset/deberta-v3-base-squad2"
     RASA_SERVER_URL: str = "http://localhost:5005"
